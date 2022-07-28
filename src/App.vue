c2450afe4e22f69ab8a16cc645dfc72ee1d666ba
Verissimo Casas <verissimo0073@gmail.com>
2022-07-28 12:57:42 -0400

    Update

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Stream from "./components/Stream.vue"
+</script>
+
+<template>
+  <img id="logo" alt="Vue logo" src="./assets/logo.png" />
+  <Stream msg="Stream Vue 3 + SP32 - EYE" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+#logo {
+  width: 90px;
+}
+</style>
---
+++ src/components/Stream.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <img class="stream-box" src="http://172.16.17.237" />
+</template>
+
+<style>
+body {
+  background-color: #1a1a1a;
+}
+h1 {
+  font-size: 1.5rem;
+  margin-bottom: 3rem;
+  color: #979797;
+  text-shadow: #585858;
+}
+.stream-box {
+  width: 640px;
+  height: 480px;
+  border-radius: 1rem;
+  box-shadow: 0 0 2em #41b883;
+}
+</style>
